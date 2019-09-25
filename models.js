59417fdd1e29707fb00c73f0f2193d231bd82048
mfhan <mf212mf@gmail.com>
2019-09-25 08:44:49 -0400

    finally

--- models.js
+++ models.js
@@ -1,6 +1,8 @@
 const Sequelize  = require('sequelize');
 
-const db = new Sequelize({
+const db = new Sequelize(
+  (process.env.DATABASE_URL || 'postgres://localhost:5432/payplay_db'),
+  {
   database: 'payplay_db',
   dialect: 'postgresql',
   define: { underscored: true }
